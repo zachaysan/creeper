e502fbce69040d805005ee1009e03ff7c82fa292
Zach Aysan <zachaysan@gmail.com>
2011-06-02 19:41:50 -0400

    user and id_str

--- lib/creeper.rb
+++ lib/creeper.rb
@@ -4,6 +4,11 @@ require "json"
 
 class Creepy
   def initialize 
+    @creepy = {}
+
+    @known_screen_names = {}
+    @known_id_strs = {}
+
     @TWITTER_API = "http://api.twitter.com/"
     @USERS = @TWITTER_API + "users/lookup.json?"
     @FOLLOWERS = @TWITTER_API + "followers/ids.json?"
@@ -22,12 +27,25 @@ class Creepy
   def tweets(options = {})
     HTTParty.get @TWEETS + find_by(options) + "&trim_user=1" + "&count=200"
   end
-  
+  def get_userid(screen_name)
+    screen_name = screen_name[:screen_name] if screen_name.is_a? Hash
+    if @known_screen_names[screen_name].nil?
+      @known_screen_names[screen_name] = users(:screen_name => screen_name)
+    end
+    @known_screen_names[screen_name]["id_str"]
+  end
+  def get_screen_name(id_str)
+    id_str = id_str[:id_str] if id_str.is_a? Hash
+    if @known_id_strs[id_str].nil?
+      @known_id_strs[id_str] = users(:id_str => id_str)
+    end
+    @known_id_strs[id_str]["screen_name"]
+  end
   protected
   
   def find_by(options = {})
-    unless options[:ids].nil?
-      input = options[:ids]
+    unless options[:id_str].nil?
+      input = options[:id_str]
     else
       input = options[:screen_name]
     end
@@ -36,7 +54,7 @@ class Creepy
     elsif input.is_a? Integer
       input = input.to_s
     end
-    unless options[:ids].nil?
+    unless options[:id_str].nil?
       return "user_id=" + input
     else 
       return "screen_name=" + input
@@ -48,7 +66,9 @@ end
 #pp Creepy.new.users(:ids => 2353)
 #pp Creepy.new.friends("zachaysan")
 #pp Creepy.new.followers("zachaysan")
-pp Creepy.new.tweets(:screen_name => "zachaysan")
+pp Creepy.new.get_userid(:screen_name => "zachaysan")
+pp Creepy.new.get_screen_name(:id_str => "18900952")
+
 #a = JSON.parse(resp)
 #pp a.size
 #pp a[0]
